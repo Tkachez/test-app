ca40f7456ee11b6b21490bdfd3a35a5f2f7b51ae
Tkachez <tkachez@gmail.com>
2020-01-16 18:33:39 +0300

    removed creating rectangles login and now a new rectangle is created each time a mousedown event occurs

--- src/index.js
+++ src/index.js
@@ -1,67 +1,85 @@
 import utils from "./modules/utils";
-import shapes from "./modules/shapes";
+import Rectangle from './modules/rectangle'
 
 window.onload = () => {
     let canvas = document.getElementById("canvas"),
         context = canvas.getContext("2d"),
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight,
-        rectangles = shapes(),
+        initialElement,
+        baseRect = {
+            y: 20,
+            x: 20,
+            width: 100,
+            height: 100
+        },
         offset = {},
-        isDragging = false,
+        activeElements = [],
         dragHandle;
 
-    let draw = () => {
+    /**
+     *
+     */
+    let drawInitialRectangle = () => {
         context.clearRect(0, 0, width, height);
-        context.fillStyle = "gray";
+        context.fillStyle = "wheat";
         context.beginPath();
-
-        rectangles.map(shape => {
-            if (isDragging && shape === dragHandle) {
-                context.shadowColor = "black";
-                context.shadowOffsetX = 4;
-                context.shadowOffsetY = 4;
-                context.shadowBlur = 8;
-            }
+        context.fillRect(20, 20, 100, 100);
+        context.fill();
+        context.strokeStyle = "black";
+        context.lineWidth = 2;
+        context.strokeRect(20, 20, 100, 100);
+        if(initialElement){
+            context.fillStyle = "wheat";
             context.beginPath();
-            context.fillRect(shape.x, shape.y, shape.width, shape.height);
+            context.fillRect(initialElement.x, initialElement.y, initialElement.width, initialElement.height);
             context.fill();
-
-            context.shadowColor = null;
-            context.shadowOffsetX = null;
-            context.shadowOffsetY = null;
-            context.shadowBlur = null;
-        });
+            context.strokeStyle = "black";
+            context.lineWidth = 2;
+            context.strokeRect(initialElement.x, initialElement.y, initialElement.width, initialElement.height);
+        }
     };
 
-    document.body.addEventListener("mousedown", function (event) {
-        rectangles.map(shape => {
+    let drawNewUnit = (movingElement) => {
+        context.fillStyle = movingElement.fillColor;
+        context.beginPath();
+        context.fillRect(movingElement.x, movingElement.y, movingElement.width, movingElement.height);
+        context.fill();
+        context.strokeStyle = movingElement.borderColor;
+        context.lineWidth = 2;
+        context.strokeRect(movingElement.x, movingElement.y, movingElement.width, movingElement.height);
+    };
 
+    let init = () => {
+        document.body.addEventListener("mousedown", (event) => {
             let onMouseMove = (event) => {
                 dragHandle.x = event.clientX - offset.x;
                 dragHandle.y = event.clientY - offset.y;
-                draw();
+                drawInitialRectangle();
+                drawNewUnit(dragHandle);
             };
 
-            let onMouseUp = (e) => {
+            let onMouseUp = () => {
                 document.body.removeEventListener("mousemove", onMouseMove);
                 document.body.removeEventListener("mouseup", onMouseUp);
-                isDragging = false;
-                draw();
+                drawInitialRectangle();
+                activeElements.push(dragHandle);
+                drawNewUnit(dragHandle);
             };
 
-            if (utils.pointInRect(event.clientX, event.clientY, shape)) {
-                isDragging = true;
+            if (utils.pointInRect(event.clientX, event.clientY, baseRect)) {
+                let movingElement = new Rectangle(20, 20, "wheat", "black");
                 document.body.addEventListener("mousemove", onMouseMove);
                 document.body.addEventListener("mouseup", onMouseUp);
-                dragHandle = shape;
-                offset.x = event.clientX - shape.x;
-                offset.y = event.clientY - shape.y;
-                draw();
+                dragHandle = movingElement;
+                offset.x = event.clientX - movingElement.x;
+                offset.y = event.clientY - movingElement.y;
+                drawInitialRectangle();
+                drawNewUnit(movingElement);
             }
 
-        })
-    });
-
-    draw();
+        });
+        drawInitialRectangle();
+    };
+    init();
 };
--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,11 +1,17 @@
 let utils = {
-    pointInRect: function(x, y, rect) {
+    pointInRect: (x, y, rect) => {
         return utils.inRange(x, rect.x, rect.x + rect.width) &&
             utils.inRange(y, rect.y, rect.y + rect.height);
     },
 
-    inRange: function(value, min, max) {
+    inRange: (value, min, max) => {
         return value >= Math.min(min, max) && value <= Math.max(min, max);
+    },
+
+    getDistance: (x1, x2, y1, y2) => {
+        let xDistance = x2 - x1,
+            yDistance = y2 - y1;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 };
 
